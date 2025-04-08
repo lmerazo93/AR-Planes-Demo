d72426f85e213757362f36f23f6f8c6689a0fb2a
Ligia Erazo <ligiaerazomacbook@Ligias-MBP.lan>
2025-04-08 10:51:39 -0700

    Fourth Commit
    Changed Feathered Plane material, added audio to makeItem function.

--- Assets/Code/MenuManager.cs
+++ Assets/Code/MenuManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     public GameObject[] items;
+    public AudioClip makeItemSound; // Assign your sound effect in the Inspector
     Transform camTrans;
 
     void Start()
@@ -12,7 +13,12 @@ public class MenuManager : MonoBehaviour
 
     public void MakeItem(int itemNum)
     {
-        Vector3 position = camTrans.position + camTrans.forward;
-        Instantiate(items[itemNum], position, Quaternion.identity);
+        if (makeItemSound != null)
+        {
+            Vector3 position = camTrans.position + camTrans.forward;
+            AudioSource.PlayClipAtPoint(makeItemSound, position);
+        }
+        Vector3 instantiatePosition = camTrans.position + camTrans.forward;
+        Instantiate(items[itemNum], instantiatePosition, Quaternion.identity);
     }
 }
